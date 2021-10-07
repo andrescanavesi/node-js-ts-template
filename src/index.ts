fea47cdeb7619719d93f86ee1609dd77105f20c5
Andres Canavesi <andres.canavesi@gmail.com>
2021-10-07 10:29:38 -0300

    init

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+require("dotenv-safe").config({ allowEmptyValues: false });
+
+console.info("Hello world");
---
+++ test/index.test.ts
@@ -0,0 +1,16 @@
+require("dotenv-safe").config({ allowEmptyValues: false });
+
+describe("My test", () => {
+  beforeAll(async () => {
+    //
+  });
+
+  afterAll(async () => {
+    //
+  });
+
+  test("Should list integrations", async () => {
+    const a = 1;
+    expect(a).toBe(1);
+  });
+});
